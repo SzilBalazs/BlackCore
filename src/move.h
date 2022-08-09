03854ad5c3a1c0bcb7e624bef239a03f82e8e57e
BuildTools <unconfigured@null.spigotmc.org>
2022-08-09 16:55:57 +0200

    Added copyright notice

--- src/move.h
+++ src/move.h
@@ -1,4 +1,20 @@
-// main source is https://www.chessprogramming.org/Encoding_Moves
+//     BlackCore is a UCI Chess engine
+//     Copyright (c) 2022 SzilBalazs
+//
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// The concept of this class is from https://www.chessprogramming.org/Encoding_Moves
 #ifndef CHESS_MOVE_H
 #define CHESS_MOVE_H
 
