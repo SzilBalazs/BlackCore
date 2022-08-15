a40e26e8e16b49990cfb4d6733f6933d89cf204f
SzilBalazs <szilprogramming@gmail.com>
2022-08-15 18:08:48 +0200

    Basic UCI parser

--- src/uci.cpp
+++ src/uci.cpp
@@ -3,6 +3,15 @@
 #include "uci.h"
 #include "tt.h"
 #include "position.h"
+#include "movegen.h"
+
+Move stringToMove(const Position& pos, const std::string &s) {
+    Move moves[200];
+    Move *endPtr = generateMoves(pos, moves, false);
+    for (Move *m = moves; m != endPtr; m+=1) {
+        if (m->str() == s) return *m;
+    }
+}
 
 void uciLoop() {
     // Identifying ourselves
@@ -11,11 +20,12 @@ void uciLoop() {
 
     // We tell the GUI what options we have
     out("option", "name", "Hash", "type", "spin", "default", 128, "min", 1, "max", 1024);
+    ttResize(128);
 
     // We have sent all the parameters
     out("uciok");
 
-    Position pos = {STARTING_FEN};
+    Position pos;
 
     while (true) {
         std::string line, command, token;
@@ -47,11 +57,35 @@ void uciLoop() {
         }
 
         else if (command == "setoption") {
-
+            if (tokens.size() >= 4) {
+                if (tokens[1] == "Hash") {
+                    ttResize(std::stoi(tokens[3]));
+                }
+            }
         }
 
-        else if (command == "position") {
+        else if (command == "position" || command == "pos") {
+            if (tokens[0] == "fen") {
+                std::string fen;
+                for (int i=1;i<tokens.size() && tokens[i] != "moves";i++) {
+                    fen += tokens[i] + ' ';
+                }
+                pos.loadPositionFromFen(fen);
+            } else if (tokens[0] == "startpos") {
+                pos.loadPositionFromFen(STARTING_FEN);
+            }
 
+            if (line.find("moves") != std::string::npos) {
+                int i = 0;
+                bool move=false;
+                while (i < tokens.size()) {
+                    if (tokens[i] == "moves") move=true;
+                    else if (move) {
+                        pos.makeMove(stringToMove(pos, tokens[i]));
+                    }
+                    i++;
+                }
+            }
         }
 
         else if (command == "go") {
