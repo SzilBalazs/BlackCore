af63426db22b59a3bd6608398700f1335dd32633
SzilBalazs <szilprogramming@gmail.com>
2022-08-18 10:22:10 +0200

    Disabled ttSave

--- src/search.cpp
+++ src/search.cpp
@@ -103,7 +103,7 @@ Score search(Position &pos, Depth depth, Score alpha, Score beta, Ply ply) {
                 recordKillerMove(m, ply);
             }
 
-            ttSave(pos.getHash(), depth, beta, BETA, m);
+            // ttSave(pos.getHash(), depth, beta, BETA, m);
             return beta;
         }
 
@@ -115,7 +115,7 @@ Score search(Position &pos, Depth depth, Score alpha, Score beta, Ply ply) {
 
     }
 
-    ttSave(pos.getHash(), depth, alpha, ttFlag, bestMove);
+    // ttSave(pos.getHash(), depth, alpha, ttFlag, bestMove);
 
     return alpha;
 }
