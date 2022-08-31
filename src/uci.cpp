14773af17664613d2dec08e4ed1f456a8f7e1e1a
SzilBalazs <szilprogramming@gmail.com>
2022-08-31 15:22:31 +0200

    Tuned pawn values Bench: 5059597

--- src/uci.cpp
+++ src/uci.cpp
@@ -80,14 +80,14 @@ void uciLoop() {
     // If tuning is defined we expose search parameters to the tuner
 #ifdef TUNE
 
-    tuneOut("PAWN_VALUE_MG", 80, 50, 200);
-    tuneOut("PAWN_VALUE_EG", 140, 100, 300);
-    tuneOut("PAWN_PASSED_BONUS_MG", 40, 0, 100);
-    tuneOut("PAWN_PASSED_BONUS_EG", 60, 0, 100);
+    tuneOut("PAWN_VALUE_MG", 86, 50, 200);
+    tuneOut("PAWN_VALUE_EG", 127, 100, 300);
+    tuneOut("PAWN_PASSED_BONUS_MG", 33, 0, 100);
+    tuneOut("PAWN_PASSED_BONUS_EG", 56, 0, 100);
     tuneOut("PAWN_DOUBLE_PENALTY_MG", -15, -100, 0);
-    tuneOut("PAWN_DOUBLE_PENALTY_EG", -25, -100, 0);
-    tuneOut("PAWN_ISOLATED_PENALTY_MG", -10, -100, 0);
-    tuneOut("PAWN_ISOLATED_PENALTY_EG", -30, -100, 0);
+    tuneOut("PAWN_DOUBLE_PENALTY_EG", -28, -100, 0);
+    tuneOut("PAWN_ISOLATED_PENALTY_MG", -13, -100, 0);
+    tuneOut("PAWN_ISOLATED_PENALTY_EG", -33, -100, 0);
 
 #endif
 
