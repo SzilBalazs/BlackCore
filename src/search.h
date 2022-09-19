e0dfced33a1650b791664175278a277098a37d70
SzilBalazs <szilprogramming@gmail.com>
2022-09-19 14:19:54 +0200

    Lowering depth Bench: 2635369

--- src/search.h
+++ src/search.h
@@ -79,7 +79,7 @@ constexpr Score ASPIRATION_DELTA = 28;
 constexpr Score ASPIRATION_BOUND = 3000;
 
 constexpr Score SEE_MARGIN = 2;
-constexpr Depth SEE_DEPTH = 6;
+constexpr Depth SEE_DEPTH = 4;
 constexpr Score SEE_DEPTH_MARGIN = 100;
 
 #endif
