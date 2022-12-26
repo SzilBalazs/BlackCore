564304589a1ac77b7c52caf3ee80b7ec7799acf7
SzilBalazs <szilprogramming@gmail.com>
2022-12-26 09:55:29 +0100

    Simplified lazy smp change Bench: 4304291

--- src/threads.h
+++ src/threads.h
@@ -86,17 +86,13 @@ struct ThreadData {
     }
 
     void updateNodesSearched(Move m, U64 totalNodes) {
-        if (threadId & 1) return;
         mNodesSearched.lock();
         nodesSearched[m.getFrom()][m.getTo()] += totalNodes;
         mNodesSearched.unlock();
     }
 
     Score scoreRootNode(Move m) {
-        if (threadId & 1)
-            return rand();
-        else
-            return nodesSearched[m.getFrom()][m.getTo()] / 1000;
+        return nodesSearched[m.getFrom()][m.getTo()] / 1000;
     }
 
     Score scoreMove(const Position &pos, Move prevMove, Move m) {
